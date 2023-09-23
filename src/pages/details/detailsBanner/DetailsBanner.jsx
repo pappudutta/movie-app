d6ded5bc93d69b6df0a9cec024fbb37caba020a3
Pappu Dutta <pappudutta279@bitbucket.org>
2023-09-23 19:24:35 +0530

    add director and writer dynamically

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -19,7 +19,13 @@ const DetailsBanner = ({ video, crew }) => {
 
   const { url } = useSelector(state => state.home);
 
-  const _genres = data?.genres.map(g => g.id);
+  const _genres = data?.genres?.map(g => g.id);
+
+  const director = crew?.filter(f => f.job === "Director");
+
+  const writer = crew?.filter(
+    f => f.job === "Screenplay" || f.job === "Story" || f.job === "Writer"
+  );
 
   const toHoursAndMinutes = totalMinutes => {
     const hours = Math.floor(totalMinutes / 60);
@@ -54,12 +60,12 @@ const DetailsBanner = ({ video, crew }) => {
                       <div className="right">
                         <div className="title">{`${
                           data?.name || data?.title
-                        } (${dayjs(data?.release_date).format("YYYY")})`}</div>
+                        } (${dayjs(data?.release_date)?.format("YYYY")})`}</div>
                         <div className="subtitle">{data?.tagline}</div>
                         <Genres data={_genres} />
                         <div className="row">
                           <CircleRating
-                            rating={data?.vote_average.toFixed(1)}
+                            rating={data?.vote_average?.toFixed(1)}
                           />
                           <div className="playbtn">
                             <PlayIcon />
@@ -84,7 +90,7 @@ const DetailsBanner = ({ video, crew }) => {
                             <div className="infoItem">
                               <span className="text bold">Release Date:</span>
                               <span className="text">
-                                {dayjs(data?.release_date).format(
+                                {dayjs(data?.release_date)?.format(
                                   "MMM D, YYYY"
                                 )}
                               </span>
@@ -99,6 +105,37 @@ const DetailsBanner = ({ video, crew }) => {
                             </div>
                           )}
                         </div>
+
+                        {director?.length > 0 && (
+                          <div className="info">
+                            <div className="text bold">
+                              <span className="text bold">Director:</span>
+                              <span className="text">
+                                {director?.map((d, i) => (
+                                  <span key={i}>
+                                    {d.name}
+                                    {director.length - 1 !== i && ", "}
+                                  </span>
+                                ))}
+                              </span>
+                            </div>
+                          </div>
+                        )}
+                        {writer?.length > 0 && (
+                          <div className="info">
+                            <div className="text bold">
+                              <span className="text bold">Writer:</span>
+                              <span className="text">
+                                {writer?.map((d, i) => (
+                                  <span key={i}>
+                                    {d.name}
+                                    {writer.length - 1 !== i && ", "}
+                                  </span>
+                                ))}
+                              </span>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </ContentWrapper>
