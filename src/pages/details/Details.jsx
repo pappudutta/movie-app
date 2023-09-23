47b92ceefe6a4846d888ade782ab5ac86bd2e41f
Pappu Dutta <pappudutta279@bitbucket.org>
2023-09-23 14:19:43 +0530

    feat: Playbtn create

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.scss";
-// import useFetch from "../../hooks/useFetch";
-// import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
-  // const { mediaType, id } = useParams();
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
   return (
     <div>
